e40dcc9067771d9dd6d3039b46a6bbd211c60826
Sumit Kumar Singh <sumitks866@gmail.com>
2020-10-10 14:31:53 +0530

    add home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
-import 'package:application_biet_biet_hacks/screens/login.dart';
 import 'package:flutter/material.dart';
+import 'package:application_biet_biet_hacks/wrapper..dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Login(),
-      ),
+      home: Wrapper(),
     );
   }
 }
