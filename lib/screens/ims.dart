255d7bc2978b7751b49a3d22adfda5e03d04e2a3
Sumit Kumar Singh <sumitks866@gmail.com>
2020-10-11 09:56:42 +0530

    remove notice page, add ims

--- lib/screens/ims.dart
+++ lib/screens/ims.dart
@@ -3,8 +3,39 @@ import 'package:flutter/material.dart';
 class Ims extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child:Text("Ims")
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon : Icon(Icons.arrow_back, color : Colors.white),
+          onPressed: (){
+            Navigator.pop(context) ;
+          },
+        ),
+        backgroundColor: Color.fromRGBO(2, 52, 130, 1),
+        title: Text("IMS", style: TextStyle(color : Colors.white),),
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(10,20,10,20),
+        child:ListView(
+          children: <Widget>[
+            Text(
+              "Institute Management System",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            RaisedButton(
+              onPressed: (){
+                Navigator.pushNamed(context, '/you');
+              },
+              child: Icon(Icons.person),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/shared/global.dart
+++ lib/shared/global.dart
@@ -1,9 +1,11 @@
 import 'package:application_biet_biet_hacks/models/student_def.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 StudentData currActiveStudent;
 CollectionReference studentRecord = FirebaseFirestore.instance.collection('students');
+String ProfilePicUrl;
 
 StudentData fetchData(DocumentSnapshot snapshot, String uid) {
   return StudentData(
@@ -21,11 +23,14 @@ StudentData fetchData(DocumentSnapshot snapshot, String uid) {
 
 Future<StudentData> initData() async {
   final String uid = FirebaseAuth.instance.currentUser.uid;
+  DocumentSnapshot snapshot = await studentRecord.doc(uid).get();
+  currActiveStudent = fetchData(snapshot, uid);
   try {
-    DocumentSnapshot snapshot = await studentRecord.doc(uid).get();
-    currActiveStudent = fetchData(snapshot, uid);
+    ProfilePicUrl = (await FirebaseStorage.instance.ref().child("${currActiveStudent.rollNumber}.jpg").getDownloadURL()).toString();
+    print(ProfilePicUrl);
   }catch(error) {
     print(error.toString());
+    ProfilePicUrl = null;
   }
   return currActiveStudent;
 }
